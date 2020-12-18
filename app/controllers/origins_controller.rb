9ec1d5975d694468ceb9061f3be8cd1cba5a13fa
tomoya-nishiyama <a.lucky0630@gmail.com>
2020-12-18 13:47:03 +0900

    投稿機能の実装

--- app/controllers/origins_controller.rb
+++ app/controllers/origins_controller.rb
@@ -1,7 +1,31 @@
 class OriginsController < ApplicationController
 
-def index
-  @origin = Origin.all
-end
+  def index
+    @origins = Origin.all
+  end
+
+  def new
+    @origin = Origin.new
+  end
+
+  def create
+    @origin = Origin.new(origin_params)
+    @origin.user_id = current_user.id
+    url = params[:origin][:youtube_url]
+    url = url.last(11)
+    @origin.youtube_url = url
+    if @origin.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def origin_params
+    params.require(:origin).permit(:book_title, :text, :youtube_url, :amazon, :category_id).merge(user_id: current_user.id)
+  end
+
 
 end
