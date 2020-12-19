a17b6fd32268d4751e56d7484f602ea44974d9c9
tomoya-nishiyama <a.lucky0630@gmail.com>
2020-12-19 22:41:00 +0900

    ユーザー新規登録の単体テストコード実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "name,email,password,password_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it "nameが空だと登録できない" do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it "emailが空では登録できない" do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it "重複したemailが存在する場合登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it "emailに＠を含まなければ登録できない" do
+        @user.email = 'tttcom'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it "passwordが空では登録できない" do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it "passwordが5文字以下であれば登録できない" do
+        @user.password = '1111'
+        @user.password_confirmation = "1111"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it "passwordは半角英数字混合でなければ登録できない" do
+        @user.password = '111111'
+        @user.password_confirmation = "111111"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+      end
+      it "passwordが存在してもpassword_confirmationが空では登録できない" do
+        @user.password = '111111'
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
 end
