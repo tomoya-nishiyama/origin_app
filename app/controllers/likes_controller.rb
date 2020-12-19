94b3cebd3532f30017ad3fdc7a75fc1f1869f919
tomoya-nishiyama <a.lucky0630@gmail.com>
2020-12-19 23:47:01 +0900

    いいね機能非同期の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(origin_id: params[:origin_id])
-    redirect_back(fallback_location: root_path)
+    @origin = Origin.find(params[:origin_id])
+    @like = current_user.likes.build(origin_id: params[:origin_id])
+    @like.save
+    @likeCounts = Like.where(origin_id: params[:origin_id])
+    # @like = current_user.likes.create(origin_id: params[:origin_id])
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @origin = Origin.find(params[:origin_id])
     @like = Like.find_by(origin_id: params[:origin_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    @likeCounts = Like.where(origin_id: params[:origin_id])
+    # @like = Like.find_by(origin_id: params[:origin_id], user_id: current_user.id)
+    # @like.destroy
+    # redirect_back(fallback_location: root_path)
   end
 
 end
