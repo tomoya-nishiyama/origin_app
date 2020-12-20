d13668fdbdeb976cc66c0fcb663906c6c769fc50
tomoya-nishiyama <a.lucky0630@gmail.com>
2020-12-20 11:45:58 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,8 +4,6 @@ class LikesController < ApplicationController
     @like = current_user.likes.build(origin_id: params[:origin_id])
     @like.save
     @likeCounts = Like.where(origin_id: params[:origin_id])
-    # @like = current_user.likes.create(origin_id: params[:origin_id])
-    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
@@ -13,9 +11,6 @@ class LikesController < ApplicationController
     @like = Like.find_by(origin_id: params[:origin_id], user_id: current_user.id)
     @like.destroy
     @likeCounts = Like.where(origin_id: params[:origin_id])
-    # @like = Like.find_by(origin_id: params[:origin_id], user_id: current_user.id)
-    # @like.destroy
-    # redirect_back(fallback_location: root_path)
   end
 
 end
