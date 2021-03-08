e4f15de3e3b65ac2e8ad5c3bb441ecef77ad1374
tomoya-nishiyama <a.lucky0630@gmail.com>
2021-03-09 00:06:02 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.95.53.119', user: 'ec2-user', roles: %w{app db web}
+server '54.65.247.185', user: 'ec2-user', roles: %w{app db web}
